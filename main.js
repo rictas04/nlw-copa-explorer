9d9f03cdecfe33d5b2a82329c4773daac4237c48
Ricardo Dantas <rictas@hotmail.com>
2022-11-11 21:59:03 -0300

    :tada: Commit Inicial

---
+++ main.js
@@ -0,0 +1,44 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="${player1}" />
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="${player2}" />
+    </li>
+  `
+}
+
+//criar um novo card pelo js
+let delay = -0.8
+function createCard(date, day, games) {
+  delay = delay + 0.8
+  return `
+    <div class="card" style="animation-delay: ${delay}s">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  //Retorna um cartão novo
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("brazil", "13:00", "switzerland")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("cameroon", "16:00", "brazil") +
+      createGame("serbia", "16:00", "switzerland")
+  )
